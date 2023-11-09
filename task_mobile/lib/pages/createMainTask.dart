8291c6046653d5a09047b7af94a2e4e5521d581c
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-11-09 10:16:42 +0530

    Update createMainTask.dart

--- task_mobile/lib/pages/createMainTask.dart
+++ task_mobile/lib/pages/createMainTask.dart
@@ -4,7 +4,6 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:task_mobile/methods/colors.dart';
 import 'package:task_mobile/taskMainPages/AllmainTaskList.dart';
 
